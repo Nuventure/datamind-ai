044ee0e470ad1cf2e593aed2d1ad3d62fd1b5f0f
Harikrishnan <harikrishnan.r@nuventure.in>
2026-01-29 15:14:51 +0530

    refactor: remove @tanstack/react-query dependency and its associated client setup.

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,17 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import MainLayout from "./components/layout/MainLayout";
 import Dashboard from "./pages/Dashboard/ui/Dashboard";
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-      retry: false,
-    },
-  },
-});
 
 const router = createBrowserRouter([
   {
@@ -28,10 +18,10 @@ const router = createBrowserRouter([
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
+    <>
       <RouterProvider router={router} />
       <Toaster position="top-right" reverseOrder={false} />
-    </QueryClientProvider>
+    </>
   );
 }
 
