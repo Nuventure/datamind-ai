fd7bbd61ed12da19a94206eb1cb6a8701b070702
Jaya krishna R <jayakrishna.r@nuventure.in>
2026-02-03 11:14:04 +0530

    updated layout ui

--- ui/src/components/layout/MainLayout.tsx
+++ ui/src/components/layout/MainLayout.tsx
@@ -1,22 +1,20 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Sidebar from './sidebar/Sidebar';
-import Header from './header/Header';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Sidebar from "./sidebar/Sidebar";
+import Header from "./header/Header";
 
 const MainLayout: React.FC = () => {
-    return (
-        <div className="flex h-screen overflow-hidden bg-gray-50">
-            <Sidebar />
-            <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
-                <Header />
-                <main className="flex-1 p-4">
-                    <div className="max-w-7xl">
-                        <Outlet />
-                    </div>
-                </main>
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex h-screen overflow-hidden bg-black text-white">
+      <Sidebar />
+      <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+        <Header />
+        <main className="flex-1 p-4">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
 };
 
 export default MainLayout;
